f925590a348169ac7a3243f5315a07175758c8c7
Olivier Deschênes <odeschenes@alix.ai>
2024-09-07 20:18:01 -0400

    WIP

---
+++ src/routes/index.tsx
@@ -0,0 +1,34 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Program } from "../components/Program";
+import { OperationsManagementAndLogistics } from "../data/data";
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
+function Component() {
+  const { user } = useUser();
+
+  return (
+    <div className={"flex flex-col gap-5"}>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      {JSON.stringify(user?.id, null, 2)}
+      <Program program={OperationsManagementAndLogistics} />
+    </div>
+  );
+}
+
+export const Route = createFileRoute("/")({
+  component: Component,
+});
