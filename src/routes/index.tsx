50b552dca81d279bd2dc2a54c7d006ea54ccfd61
Olivier Deschênes <olivierdeschenes9@gmail.com>
2024-10-26 22:28:40 -0400

    Remove auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Program } from "../components/Program";
 import { OperationsManagementAndLogistics } from "../data/data";
-import {
-  SignedOut,
-  SignInButton,
-  SignedIn,
-  UserButton,
-  useUser,
-} from "@clerk/clerk-react";
 
 function Component() {
-  const { user } = useUser();
-
   return (
     <div className={"flex flex-col gap-5"}>
-      <header>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-      {JSON.stringify(user?.id, null, 2)}
       <Program program={OperationsManagementAndLogistics} />
     </div>
   );
